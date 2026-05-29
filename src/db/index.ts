a5f7b8164b6c8eb93dc5a34887277c43b0990e04
keyurgandhi <keyur21.gandhi@gmail.com>
2026-05-29 03:19:23 -0500

    db connection fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,8 +9,17 @@ if (!process.env.DATABASE_URL) {
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
+console.log("DB URL ---- ",process.env.DATABASE_URL);
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  // ADD THE FIX HERE:
+  ssl: {
+    // Tells the Node app to encrypt data without requiring a local physical certificate file
+    rejectUnauthorized: false 
+  }
+});
 export const db = drizzle(pool, { schema });
 
 export * from "./schema";
